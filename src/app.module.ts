a8d81483af7c42cd0674bc3a0251d0586b94e74d
Salem Lokmani <salemlokmani99@gmail.com>
2022-01-31 01:15:52 +0100

    CRUD & Auth shopit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule,
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ProductModule,
+    UserModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
